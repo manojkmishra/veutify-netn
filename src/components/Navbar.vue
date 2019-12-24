4f26a958fe697c57ff49323a89f3945903005385
manojkmishra <manojmishra571980@gmail.com>
2019-12-25 00:07:43 +1100

    toolbar done

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <v-app-bar flat app class="grey lighten-4">
+            <v-toolbar-title class="grey--text"> 
+                <span class="font-weight-light">Saw</span>
+                <span>Analytics</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn text color="grey">
+                <span>signout</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-app-bar>
+    </nav>
+</template>
+<script>
+export default {
+    
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="home">
     <h1>This is the homepage</h1>
-    <v-divider></v-divider>
-    <v-btn>click me1</v-btn> &nbsp;
-    <v-btn class="pink white--text">click me2</v-btn>&nbsp;
-    <v-btn depressed color="pink">click me3</v-btn>&nbsp;
-    <v-btn text color="pink">click me4</v-btn><br><br>
-
-    <v-btn depressed class="pink white--text">
-      <v-icon left>email</v-icon>
-      <span>email me</span>
-    </v-btn>&nbsp;
-
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>email</v-icon>
-      <span>email me</span>
-    </v-btn>&nbsp;
-
-    <v-btn depressed large class="pink white--text">
-      <span>email me</span>
-      <v-icon right large>email</v-icon>
-    </v-btn><br><br>
-
-    <v-btn fab dark small depressed color="purple">
-      <v-icon dark>favorite</v-icon>
-    </v-btn><br><br>
 
   </div>
 </template>
