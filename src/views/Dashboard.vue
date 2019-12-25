fac7f2b6b3a0e0c052cc0bca00357fa660365aa4
manojkmishra <manojmishra571980@gmail.com>
2019-12-26 02:08:10 +1100

    Grid

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,17 +2,14 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <v-layout row wrap>
-         <v-flex xs12 md6><v-btn outlined block >1</v-btn></v-flex>
-         <v-flex xs4 md2><v-btn outlined block >2</v-btn></v-flex>
-         <v-flex xs4 md2><v-btn outlined block >2</v-btn></v-flex>
-         <v-flex xs4 md2><v-btn outlined block >2</v-btn></v-flex>
-      </v-layout>
-      <br>
-       <v-layout row wrap justify-space-around>
-         <v-flex xs4 md3><v-btn outlined block >43</v-btn></v-flex>
-         <v-flex xs4 md3><v-btn outlined block >2</v-btn></v-flex>
-      </v-layout>
+      <v-card flat class="pa-3">
+          <v-layout row wrap>
+            <v-flex xs12 md6> <div class="caption grey--text">Project title</div>  <div> Create a new Website</div>  </v-flex>
+            <v-flex xs6 sm4 md2> <div class="caption grey--text">Person</div> <div> Myself</div> </v-flex>
+            <v-flex xs6 sm4 md2><div class="caption grey--text">Due by</div> <div> 1 Jan 2020</div> </v-flex>
+            <v-flex xs2 sm4 md2> <div class="caption grey--text">Status</div> <div> Ongoing</div> </v-flex>
+          </v-layout>
+      </v-card>
     </v-container>
 
   </div>
