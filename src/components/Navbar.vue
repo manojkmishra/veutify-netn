1fb662e12ef6ebed8919d41b393d64101800b794
manojkmishra <manojmishra571980@gmail.com>
2019-12-25 23:31:14 +1100

    drawer lists

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,13 +12,27 @@
                 <v-icon right>exit_to_app</v-icon>
             </v-btn>
         </v-app-bar>
-        <v-navigation-drawer v-model="drwr" app  class="primary">
-            <p>Test</p>
+        <v-navigation-drawer v-model="drwr" app  class="success">
+               <v-list rounded>
+                    <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                        <v-list-item-icon>
+                            <v-icon >{{ link.icon }}</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+              </v-list>
         </v-navigation-drawer>
     </nav>
 </template>
 <script>
 export default {
-    data(){return{drwr:false}}
+    data(){return{drwr:false, links: [
+        { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
+        { icon: 'mdi-account', text: 'Team', route: '/team' },
+      ]}
+    }
 }
 </script>
