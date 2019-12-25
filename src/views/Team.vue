1daa2e90da14a9037f49435b27805dc9358dd417
manojkmishra <manojmishra571980@gmail.com>
2019-12-26 01:11:34 +1100

    customer style

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="team">
-    <h1>Team</h1>
-
+    <h1 class="subheading grey--text">Team</h1>
+      <v-container class="my-5">
+          <div class="man"> dsadsad  </div>
+     content
+    </v-container>
   </div>
 </template>
+<style scoped>
+.man{
+background-color: red
+}
+</style>
