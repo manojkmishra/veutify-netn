dacd391cd8c8114c2b114ea8a5dc84a6eb37e7eb
manojkmishra <manojmishra571980@gmail.com>
2019-12-26 21:02:09 +1100

    v-card

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,13 +2,35 @@
   <div class="team">
     <h1 class="subheading grey--text">Team</h1>
       <v-container class="my-5">
-          <div class="man"> dsadsad  </div>
-     content
+         <v-layout row wrap>
+             <v-flex xs-12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+                <v-card flat class="text-xs-center ma-3">
+                    <v-responsive class="pt-4">image will come here</v-responsive>
+                    <v-card-text>
+                        <div class="v-subheading">{{person.name}}</div>
+                        <div class="grey--text">{{person.role}}</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn text color="grey">
+                            <v-icon small left>mdi-message</v-icon>
+                            <span>Message</span>
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+             </v-flex>
+         </v-layout>
     </v-container>
   </div>
 </template>
-<style scoped>
-.man{
-background-color: red
+<script>
+export default {
+    data(){ return{ team: [ { name: 'The Net Ninja', role: 'Web developer' },
+                            { name: 'Ryu', role: 'Graphic designer' },
+                            { name: 'Chun Li', role: 'Web developer' },
+                            { name: 'Gouken', role: 'Social media maverick' },
+                            { name: 'Yoshi', role: 'Sales guru'}
+                          ]
+                    }
+        }
 }
-</style>
+</script>
