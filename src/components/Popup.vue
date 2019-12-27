4f8b33672d74950dcfa58f058553786bcb7b2c2f
manojkmishra <manojmishra571980@gmail.com>
2019-12-27 14:07:59 +1100

    validation

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -7,18 +7,16 @@
       <v-card>
         <v-card-title class="headline grey lighten-2" primary-title>Add a New Project</v-card-title>
         <v-card-text>
-            <v-form class="px-3">
-                <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
-                <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil"></v-textarea>
-           
-                    <v-menu max-width="290">
+            <v-form class="px-3" ref="form">
+                <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder" :rules="inputRules"></v-text-field>
+                <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil" :rules="inputRules"></v-textarea>
+                  <v-menu max-width="290">
                       <template v-slot:activator="{ on }">
                             <v-text-field :value="formattedDate" label="Due date" 
-                            prepend-icon="mdi-calendar-range" v-on="on"></v-text-field>
+                            prepend-icon="mdi-calendar-range" v-on="on" :rules="inputRules"></v-text-field>
                       </template>
                       <v-date-picker v-model="due"></v-date-picker>
                     </v-menu>
-                 
                 <v-btn text class="success mx-0 mt-3" @click="submit">Add Project</v-btn>
             </v-form>
         </v-card-text>
@@ -30,9 +28,11 @@
 import format from 'date-fns/format'
  import parseISO from 'date-fns/parseISO'
 export default {
-    data(){  return{title:'', content:'', due:''}    },
+    data(){  return{title:'', content:'', due:'', inputRules:[v=>v.length>=3||'Min len is 3 chars']}    },
     methods:{
-        submit(){console.log('form items=',this.title,this.content, this.due)}  // eslint-disable-line
+        submit(){ if(this.$refs.form.validate())
+                    console.log('form items=',this.title,this.content, this.due)   // eslint-disable-line
+            }  
     },
     computed:{formattedDate(){return this.due ? format(parseISO(this.due),'do MMM yyyy') : ''}}
 }
